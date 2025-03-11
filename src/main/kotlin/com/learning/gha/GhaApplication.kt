00a5b976218f36b190ca28daa0aa7bd784239b24
aassalax <aassali.dev@gmail.com>
2025-03-11 13:06:43 +0100

    first commit

---
+++ src/main/kotlin/com/learning/gha/GhaApplication.kt
@@ -0,0 +1,11 @@
+package com.learning.gha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GhaApplication
+
+fun main(args: Array<String>) {
+	runApplication<GhaApplication>(*args)
+}
---
+++ src/test/kotlin/com/learning/gha/GhaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learning.gha
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GhaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
